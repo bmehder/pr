6fb1135884ba8d1fde2e406c93fe6c643f2dea7e
bmehder <bmehder@gmail.com>
2022-10-30 00:18:13 +0200

    added faqs fetch to root layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,6 @@
 import type { RequestEvent } from '@sveltejs/kit'
+type Category = { id: string; name: string }
+type SortedFaq = { name: string; faqs: { title: string; content: string }[] }
 
 export const prerender = true
 
@@ -8,6 +10,29 @@ export async function load({ fetch }: RequestEvent) {
     `https://sprucehealthgroup.com/wp-json/wp/v2/posts?per_page=100&_embed`
   )
   const items = await postsRes.json()
+
+  const BASE_URL = 'https://sprucehealthgroup.com/wp-json/wp/v2/'
+  const FAQ_CATS = 'faq-cats'
+  const FAQS_BY_CAT = 'faqs?faq-cats='
+
+  const faqCatsRes = await fetch(`${BASE_URL}${FAQ_CATS}`)
+  const faqCats = await faqCatsRes.json()
+
+  const catIds = faqCats.map((cat: Category) => cat.id)
+
+  const getFaqsByCatsId = catIds.map((id: number) =>
+    fetch(`${BASE_URL}${FAQS_BY_CAT}${id}`)
+  )
+  const faqsByCatsRes = await Promise.all(getFaqsByCatsId)
+  const faqsByCats = await Promise.all(faqsByCatsRes.map(res => res.json()))
+
+  const sortedFaqs: SortedFaq[] = faqCats.reduce(
+    (acc: SortedFaq[], item: Category, index: number) => {
+      acc = [...acc, { name: item.name, faqs: faqsByCats[index] }]
+      return acc
+    },
+    []
+  )
 
   const slides = [
     {
@@ -32,5 +57,5 @@ export async function load({ fetch }: RequestEvent) {
     },
   ]
 
-  return { items, slides }
+  return { items, slides, sortedFaqs }
 }
