1c159be9e525030c30c56340d633072fed197aa5
bmehder <bmehder@gmail.com>
2022-10-29 03:38:55 +0200

    fetch posts in root layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,29 +1,34 @@
+import type { RequestEvent } from '@sveltejs/kit'
+
 export const prerender = true
 
 /** @type {import('./$types').PageLoad} */
-export function load() {
-  return {
-    slides: [
-      {
-        src: 'img/plant.png',
-        text: 'Hello Slider 1',
-        options: { top: '50%', left: '50%' },
-      },
-      {
-        src: 'img/plant2.png',
-        text: 'Hello Slider 2',
-        options: { top: '50%', left: '50%' },
-      },
-      {
-        src: 'img/plant3.png',
-        text: 'Hello Slider 3',
-        options: { top: '50%', left: '50%' },
-      },
-      {
-        src: 'img/plant4.png',
-        text: 'Hello Slider 4',
-        options: { top: '38%', left: '38%' },
-      },
-    ],
-  }
+export async function load({ fetch }: RequestEvent) {
+  const res = await fetch(`https://sprucehealthgroup.com/wp-json/wp/v2/posts?_embed`)
+  const items = await res.json()
+
+  const slides = [
+    {
+      src: 'img/plant.png',
+      text: 'Hello Slider 1',
+      options: { top: '50%', left: '50%' },
+    },
+    {
+      src: 'img/plant2.png',
+      text: 'Hello Slider 2',
+      options: { top: '50%', left: '50%' },
+    },
+    {
+      src: 'img/plant3.png',
+      text: 'Hello Slider 3',
+      options: { top: '50%', left: '50%' },
+    },
+    {
+      src: 'img/plant4.png',
+      text: 'Hello Slider 4',
+      options: { top: '38%', left: '38%' },
+    },
+  ]
+
+  return { items, slides }
 }
--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,9 +1,9 @@
-import type { RequestEvent } from '@sveltejs/kit'
+// import type { RequestEvent } from '@sveltejs/kit'
 
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch }: RequestEvent) {
-  const res = await fetch(`https://sprucehealthgroup.com/wp-json/wp/v2/posts?_embed`)
-  const items = await res.json()
+// /** @type {import('./$types').PageLoad} */
+// export async function load({ fetch }: RequestEvent) {
+//   const res = await fetch(`https://sprucehealthgroup.com/wp-json/wp/v2/posts?_embed`)
+//   const items = await res.json()
 
-  return { items }
-}
+//   return { items }
+// }
