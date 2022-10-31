2b8d1180547609bcd2a60e3adcae5752e083fa29
bmehder <bmehder@gmail.com>
2022-10-31 14:22:25 +0100

    fetch 100 products

--- src/routes/shop/+page.ts
+++ src/routes/shop/+page.ts
@@ -2,7 +2,7 @@ import type { RequestEvent } from '@sveltejs/kit'
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }: RequestEvent) {
-  const res = await fetch('https://dummyjson.com/products?limit=32')
+  const res = await fetch('https://dummyjson.com/products?limit=100')
   const items = await res.json()
 
   return { items }
