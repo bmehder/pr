83acc6625fcda14225cb6742d68c5e42635a5fcf
bmehder <bmehder@gmail.com>
2022-11-12 18:34:09 +0100

    added live search on Shop

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -2,6 +2,7 @@
   import type { PageData } from './$types'
 
   import ProductCard from './ProductCard.svelte'
+  import MiniCard from './MiniCard.svelte'
 
   type Products = {
     products?: []
@@ -10,13 +11,12 @@
   export let data: PageData
 
   let value = ''
-  let searchQuery = ''
   let searchResults: Products | null
 
   const getSearchResults = async (value: string) => {
+    if (value.length < 2) return
     searchResults = null
 
-    searchQuery = value
     const res = await fetch(
       `https://dummyjson.com/products/search?q=${value}&limit=100`
     )
@@ -25,59 +25,35 @@
     searchResults = results
   }
 
-  const clearSearch = () => {
-    value = ''
-    searchResults = null
-  }
-
-  const handleKeypress = (evt: KeyboardEvent) => {
-    if (evt.key === 'Enter') {
-      getSearchResults(value)
-    }
-  }
-  $: !value && (searchResults = null)
+  $: if (value.length < 2) searchResults = null
 </script>
 
 <header class="search">
-  {#if searchResults && value}
-    <h1>
-      {searchResults.total} search results for '{searchQuery}'
-      <span on:click={clearSearch} on:keypress>Clear Search</span>
-    </h1>
-  {:else}
-    <h1>Shop</h1>
-  {/if}
+  <h1>Shop</h1>
   <div>
     <div>
-      <input type="text" bind:value on:keypress={handleKeypress} />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        class="w-5 h-5"
-        on:click={clearSearch}
-        on:keypress
-      >
-        <path
-          d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
-        />
-      </svg>
+      <input
+        type="text"
+        bind:value
+        placeholder="Search..."
+        on:input={() => getSearchResults(value)}
+      />
+      <aside>
+        {#if searchResults?.products}
+          {#each searchResults.products as product}
+            <MiniCard {product} />
+          {:else}
+            <p>No results</p>
+          {/each}
+        {/if}
+      </aside>
     </div>
-    <button on:click={() => getSearchResults(value)} on:keypress>Search</button>
   </div>
 </header>
 <div class="auto-grid">
-  {#if searchResults?.products}
-    {#each searchResults.products as product}
-      <ProductCard {product} />
-    {:else}
-      <p>No results</p>
-    {/each}
-  {:else}
-    {#each data.items.products as product}
-      <ProductCard {product} />
-    {/each}
-  {/if}
+  {#each data.items.products as product}
+    <ProductCard {product} />
+  {/each}
 </div>
 
 <style>
@@ -91,48 +67,31 @@
     justify-content: space-between;
     align-items: center;
   }
-  input,
-  button {
+  input {
     padding: 0.5rem 1rem;
     font-size: 1rem;
+    width: 100%;
   }
   header div div {
     position: relative;
+    width: 300px;
   }
-  svg {
+  aside {
     position: absolute;
-    top: 50%;
-    right: 0.25rem;
-    width: 1.5rem;
-    transform: translateY(-50%);
-    cursor: pointer;
+    width: 100%;
+    box-shadow: var(--shadow);
   }
-  button {
-    background-color: var(--light);
-    color: white;
-    border: 2px solid var(--light);
-    cursor: pointer;
+  aside > :global(article:nth-child(even)) {
+    background-color: #f1f1f1;
   }
   h1 {
     margin: 0;
   }
-  h1 span {
-    font-size: 1rem;
-    font-weight: normal;
-    color: var(--light);
-    text-decoration: underline;
-    cursor: pointer;
+  p {
+    padding: 1rem;
+    background-color: white;
   }
-  h1 span:hover {
-    text-decoration: none;
-  }
-  /* .content {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(18em, 1fr));
-    gap: 3rem;
-    margin-top: 1.5rem;
-  } */
-  @media screen and (max-width: 720px) {
+  @media screen and (max-width: 769px) {
     header {
       flex-direction: column;
     }
---
+++ src/routes/shop/MiniCard.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { Format } from '$lib/utils'
+
+  type Product = {
+    id: number
+    thumbnail: string
+    title: string
+    price: number
+    category: string
+    description: string
+  }
+
+  export let product: Product
+
+  const { id, thumbnail, title, price, category, description } = product
+</script>
+
+<svelte:head>
+  <title>Shop</title>
+</svelte:head>
+
+<article>
+  <div>
+    <a href="/shop/{id}"><img src={thumbnail} alt={title} /></a>
+    <div>
+      <h2><a href="/shop/{id}">{title}</a></h2>
+      <h2>{Format.toDollars(price)}</h2>
+    </div>
+  </div>
+</article>
+
+<style>
+  article {
+    position: relative;
+    max-width: 300px;
+    background-color: white;
+  }
+  div {
+    display: flex;
+    gap: 1rem;
+  }
+  div div {
+    flex-direction: column;
+    justify-content: center;
+    gap: 0;
+  }
+  h2 {
+    font-size: 1rem;
+  }
+  h2 {
+    margin: 0;
+  }
+  img {
+    width: 100px;
+    padding: 0.25rem;
+    padding-left: 0;
+    aspect-ratio: 1;
+    object-fit: cover;
+  }
+</style>
