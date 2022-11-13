123aeb36964ad1fa7cf3565a7d71a73e75196c7d
bmehder <bmehder@gmail.com>
2022-11-13 23:35:44 +0100

    turned off prerendering

---
+++ src/routes/api/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+  <title>API</title>
+</svelte:head>
+
+<h1>API</h1>
+<p>Get stuff...</p>
--- src/routes/api/f1/+server.ts
+++ src/routes/api/f1/+server.ts
@@ -1,10 +1,12 @@
 import { json } from '@sveltejs/kit'
 
-const champions = [
-  { 2022: 'Max Verstappen' },
-  { 2021: 'Max Verstappen' },
-  { 2020: 'Lewis Hamilton' },
-]
+const champions = {
+  data: [
+    { year: 2022, champ: 'Max Verstappen' },
+    { year: 2021, champ: 'Max Verstappen' },
+    { year: 2020, champ: 'Lewis Hamilton' },
+  ],
+}
 
 /** @type {import('./$types').RequestHandler} */
 export function GET() {
